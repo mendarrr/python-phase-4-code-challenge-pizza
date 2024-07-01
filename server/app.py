7947b45b3fd0e943e995a8cfa1fd0cb93cb5b284
mendarrr <sarahabby541@gmail.com>
2024-07-01 17:51:34 +0300

    Completed Code Challenge

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response, jsonify
-from flask_restful import Api, Resource
+from flask import Flask, request, jsonify
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -14,71 +13,55 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
-api = Api(app)
 
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
 
-# Retrieving restaurants with GET request
-@app.route("/restaurants", methods=['GET'])
+@app.route("/restaurants", methods=["GET"])
 def get_restaurants():
     restaurants = Restaurant.query.all()
-    return jsonify([{
-        "id": r.id, 
-        "name": r.name, 
-        "address": r.address
-        } 
-        for r in restaurants])
+    return jsonify([restaurant.to_dict(only=("id", "name", "address")) for restaurant in restaurants]), 200
 
-# Retrieving one restaurant using its id
-@app.route("/restaurants/<int:id>", methods=['GET'])
-def get_restaurant_by_id(id):
+@app.route("/restaurants/<int:id>", methods=["GET", "DELETE"])
+def restaurant_details(id):
     restaurant = db.session.get(Restaurant, id)
-    if not restaurant:
+    if request.method == "GET":
+        if restaurant:
+            return jsonify(restaurant.to_dict()), 200
         return jsonify({"error": "Restaurant not found"}), 404
-    
-    return jsonify({
-        "id": restaurant.id,
-        "name": restaurant.name,
-        "address": restaurant.address,
-        "restaurant_pizzas": [{
-            "id": rp.id,
-            "pizza": {
-                "id": rp.pizza.id,
-                "name": rp.pizza.name,
-                "ingredients": rp.pizza.ingredients,
-            },
-            "price": rp.price
-        } for rp in restaurant.pizzas]
-    })
 
-# Deleting a restaurant using its id
-@app.route("/restaurants/<int:id>", methods=['DELETE'])
-def delete_restaurant(id):
-    restaurant = db.session.get(Restaurant, id)
-    if not restaurant:
-        return jsonify({"error": "Restaurant not found"}), 404
-    
-    db.session.delete(restaurant)
-    db.session.commit()
-    
-    return '', 204
-
-# Retrieving pizzas with GET request
-@app.route("/pizzas", methods=['GET'])
+    if request.method == "DELETE":
+       if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        return "", 204
+    return jsonify({"error": "Restaurant not found"}), 404
+@app.route("/pizzas", methods=["GET"])
 def get_pizzas():
     pizzas = Pizza.query.all()
-    return jsonify([{
-        "id": p.id,
-        "name": p.name,
-        "ingredients": p.ingredients
-    } for p in pizzas]), 200
+    return jsonify([pizza.to_dict(only=("id", "name", "ingredients")) for pizza in pizzas]), 200
 
-# Creating a new pizza recipe
+# Create new restaurant pizza
+@app.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    data = request.get_json()
+    try:
+        new_restaurant_pizza = RestaurantPizza(
+            price=data["price"],
+            pizza_id=data["pizza_id"],
+            restaurant_id=data["restaurant_id"]
+        )
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+        return jsonify(new_restaurant_pizza.to_dict()), 201
+    except ValueError as e:
+        db.session.rollback()
+        return jsonify({"errors": ["validation errors"]}), 400
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"errors": ["validation errors"]}), 400
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,10 +21,12 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-    pizzas = db.relationship("RestaurantPizza", backref="restaurant", cascade="all, delete-orphan" )
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade="all, delete")
 
     # add serialization rules
-    serialize_rules = ("-pizzas.restaurant")
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
+
+    restaurants = association_proxy('restaurant_pizzas', 'pizza', creator=lambda pizza_obj: RestaurantPizza(pizza=pizza_obj))
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -38,10 +40,12 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-    restaurants = db.relationship('RestaurantPizza', backref='pizza', cascade='all, delete-orphan')
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade="all, delete")
 
     # add serialization rules
-    serialize_rules = ("-restaurant.pizzas")
+    serialize_rules = ('-restaurant_pizzas.pizza',)
+
+    pizzas = association_proxy('restaurant_pizzas', 'restaurant', creator=lambda restaurant_obj: RestaurantPizza(restaurant=restaurant_obj))
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -54,15 +58,18 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
-    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
 
     # add serialization rules
-    serialize_rules = ("-restaurant.pizzas", "-pizza.restaraunts")
+    serialize_rules = ('-pizza.restaurant_pizzas', '-restaurant.restaurant_pizzas')
 
     # add validation
     @validates('price')
-    def validate_price(self, key, price):
+    def price_validation(self, key, price):
         if price < 1 or price > 30:
             raise ValueError("Price must be between 1 and 30")
         return price
